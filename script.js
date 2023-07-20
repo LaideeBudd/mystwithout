b5215446df9114fca4b39a03bcec73558a95ec55
LaideeBudd <107522099+LaideeBudd@users.noreply.github.com>
2023-07-20 17:50:49 -0500

    fixed nav disappearing after viewing menu in mobile

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ const menuToggle = () => {
     overlay.style.display = "none";
   }
 };
+
+const navFlex = () => {
+  if (document.body.clientWidth > 768) {
+    navItems.style.display = "flex";
+    overlay.style.display = "none";
+    console.log("test");
+  } else {
+    navItems.style.display = "none";
+    overlay.style.display = "none";
+  }
+};
+
+window.onresize = navFlex;
